81a5997227cb9c8086b3cebf305e539eb2bf1990
Joel McCoy <joelmccoy@gmail.com>
2013-11-23 02:32:23 -0500

    Stop getting videos every time script starts

--- daas.py
+++ daas.py
@@ -1,13 +1,15 @@
+import os
 from death_extractor import youtube as yt
 from death_extractor import set_interval
 from death_extractor import extract_and_upload
     
-def death_as_a_service(vid_path = 'vids', post_interval=3600*2, search_interval=3600*12, dl_interval=3600*6, max_downloads=5, to_imgur=True, to_tumblr=True):
+def death_as_a_service(vid_path = 'vids', post_interval=3600*2, search_interval=3600*6, dl_interval=3600*3, max_downloads=4, to_imgur=True, to_tumblr=True):
   """Run periodic search/download/extract_and_upload operations"""
-  #print "Fetching new videos and consolidating queue..."
-  #yt.populate_queue()
-  #print "Downloading up to",max_downloads,"videos..."
-  #yt.dl(max_downloads)
+  print "Fetching new videos and consolidating queue..."
+  yt.populate_queue()
+  if len([file for file in os.listdir(vid_path) if not file.endswith('part') and not file.startswith('.')]) < 4:
+    print "Downloading up to",max_downloads,"videos..."
+    yt.dl(max_downloads)
   extract_and_upload(vid_path, to_imgur, to_tumblr)
   
   if search_interval:
