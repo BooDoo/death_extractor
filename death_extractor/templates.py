9b5916059a481ac3a7f8335a6b7f4fdf16c89ca5
Joel McCoy <joelmccoy@gmail.com>
2013-11-23 02:03:27 -0500

    Adding skull UI templates

--- death_extractor/templates.py
+++ death_extractor/templates.py
@@ -1,9 +1,9 @@
 import cv2
 
 _templates = {
-'43':  [cv2.imread('templates/gameover_43.png',0), cv2.imread('templates/leaderboard_43.png',0), cv2.imread('templates/leaderboard-vines_43.png',0)], 
-'85':  [cv2.imread('templates/gameover_85.png',0), cv2.imread('templates/leaderboard_85.png',0), cv2.imread('templates/leaderboard-vines_85.png',0)],
-'169': [cv2.imread('templates/gameover.png',0),    cv2.imread('templates/leaderboard.png',0),    cv2.imread('templates/leaderboard-vines.png',0)]
+'43':  [cv2.imread('templates/gameover_43.png',0), cv2.imread('templates/leaderboard_43.png',0), cv2.imread('templates/leaderboard-vines_43.png',0), cv2.imread('templates/skull_43.png',0)], 
+'85':  [cv2.imread('templates/gameover_85.png',0), cv2.imread('templates/leaderboard_85.png',0), cv2.imread('templates/leaderboard-vines_85.png',0), cv2.imread('templates/skull_85.png',0)],
+'169': [cv2.imread('templates/gameover_169.png',0),cv2.imread('templates/leaderboard_169.png',0),cv2.imread('templates/leaderboard-vines_169.png',0),cv2.imread('templates/skull_169.png',0)]
 }
 
 def get_templates(key="169"):
