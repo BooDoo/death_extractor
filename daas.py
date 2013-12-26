8894e34a9be747c575284232abfa65d960cda4f8
Joel McCoy <joelmccoy@gmail.com>
2013-12-25 19:32:36 -0500

    Queue, dl, and post each tick to be safe.

--- daas.py
+++ daas.py
@@ -3,20 +3,14 @@ from death_extractor import youtube as yt
 from death_extractor import set_interval
 from death_extractor import extract_and_upload
     
-def death_as_a_service(vid_path = 'vids', post_interval=3600*2, search_interval=3600*6, dl_interval=3600*3, max_downloads=4, to_imgur=False, to_tumblr=True):
+def death_as_a_service(vid_path = 'vids', max_downloads=4, to_imgur=False, to_tumblr=True):
   """Run periodic search/download/extract_and_upload operations"""
   print "Fetching new videos and consolidating queue..."
   yt.populate_queue()
   yt.dl(max_downloads)
   extract_and_upload(vid_path, to_imgur=to_imgur, to_tumblr=to_tumblr)
-  
-  if search_interval:
-    search_timer = set_interval(search_interval, yt.populate_queue)
-  if dl_interval:
-    dl_timer = set_interval(dl_interval, yt.dl, max_downloads)
-  if post_interval:
-    post_timer = set_interval(post_interval, extract_and_upload, vid_path, to_imgur=to_imgur, to_tumblr=to_tumblr)
      
 if __name__ == '__main__':
   print "Running from console..."
   death_as_a_service()
+  post_timer = set_interval(3600*2, death_as_a_service)
