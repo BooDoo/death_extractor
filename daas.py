619d558d839dfb752876a57842451f7ac35e49d7
Joel McCoy <joelmccoy@gmail.com>
2014-01-02 21:01:00 -0500

    Pass to_snapchat from daas

--- daas.py
+++ daas.py
@@ -8,7 +8,7 @@ def death_as_a_service(vid_path = 'vids', max_downloads=4, to_imgur=False, to_tu
   print "Fetching new videos and consolidating queue..."
   yt.populate_queue()
   yt.dl(max_downloads)
-  extract_and_upload(vid_path, to_imgur=to_imgur, to_tumblr=to_tumblr)
+  extract_and_upload(vid_path, to_imgur=to_imgur, to_tumblr=to_tumblr, to_snapchat=to_snapchat)
      
 if __name__ == '__main__':
   print "Running from console..."
