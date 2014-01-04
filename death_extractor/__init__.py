13fd6d21e94c72dfd4df0a24fb60a3b65dc89626
Joel McCoy <joelmccoy@gmail.com>
2014-01-03 19:31:12 -0500

    Fix traceback.print_tb

--- death_extractor/__init__.py
+++ death_extractor/__init__.py
@@ -180,8 +180,8 @@ def extract_and_upload(vid_path = 'vids', out_frame_skip=3, out_duration=4, use_
   except (cv2.error, OSError, IOError, TypeError, AttributeError) as e:
     print e
     exc_type, exc_value, exc_traceback = sys.exc_info()
-    sys.stdout.write(traceback.print_tb(exc_traceback, limit=None, file=sys.stdout))
-    sys.stdout.flush()
+    traceback.print_tb(exc_traceback, limit=None, file=sys.stderr)
+    sys.stderr.flush()
     print "\nSkipping",vid.input_file,"due to failure to extract (probably)\nmoving to problems/",vid.input_file_tail
     os.rename(vid.input_file, "problems/" + vid.input_file_tail)
     util.recall(extract_and_upload) #limit how many retries?
